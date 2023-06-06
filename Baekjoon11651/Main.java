e831d385bb8baa659693f747811384048116d15d
moonsh <118095491+fleur75921@users.noreply.github.com>
2023-06-06 13:43:10 +0900

    Create Main.java

---
+++ Baekjoon11651/Main.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static class Point implements Comparable<Point> {
+		int x;
+		int y;
+		
+		Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		@Override
+		public int compareTo(Point p) {
+			if(y != p.y) {
+				return y-p.y;
+			} else {
+				return x-p.x;
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		ArrayList<Point> list = new ArrayList<>();
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<N;i++) {
+			st = new StringTokenizer(br.readLine());
+			list.add(new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+		}
+		list.sort(null);
+		Point point;
+		for(int i=0;i<N;i++) {
+			point = list.get(i);
+			sb.append(point.x+" "+point.y+"\n");
+		}
+		System.out.println(sb);
+		br.close();
+	}
+}
