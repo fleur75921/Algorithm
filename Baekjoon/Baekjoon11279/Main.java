729c2c324c9c8c096232a25d2df837592c35c1f3
moonsh <118095491+fleur75921@users.noreply.github.com>
2023-06-21 00:05:48 +0900

    Create Main.java

---
+++ Baekjoon/Baekjoon11279/Main.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int x;
+		PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<N;i++) {
+			x = Integer.parseInt(br.readLine());
+			if(x == 0) {
+				if(q.isEmpty()) {
+					sb.append(0+"\n");
+				} else {
+					sb.append(q.poll()+"\n");
+				}
+			} else {
+				q.offer(x);
+			}
+		}
+		System.out.print(sb);
+		br.close();
+	}
+}
